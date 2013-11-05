9a7b56c3da8572b7e951840470590763a7ff3faa
soum <somugus@gmail.com>
2013-11-05 15:25:52 -0500

    android

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,10 +3,19 @@ $(function(){
 if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
     if (browserRatio >=1.5) {
         //$container.css('min-height', '360px');
-		alert('min-height 360px')
+		//alert('min-height 360px')
+		$('body').css('background', 'green')
     } else {
         //$container.css('min-height', '555px');
+		$('body').css('background', 'red')
     }
 }
-alert('test')
+
+var ua = navigator.userAgent.toLowerCase();
+var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+if(isAndroid) {
+  // Do something!
+  // Redirect to Android-site?
+  alert('android')
+}
 });
