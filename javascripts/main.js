39134c775789724da8d74a367e1178109291934a
soum <somugus@gmail.com>
2013-11-05 15:21:47 -0500

    commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
 //console.log('This would be the main JS file.');
+$(function(){
+if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
+    if (browserRatio >=1.5) {
+        //$container.css('min-height', '360px');
+		alert('min-height 360px')
+    } else {
+        //$container.css('min-height', '555px');
+    }
+}
+alert('test')
+});
