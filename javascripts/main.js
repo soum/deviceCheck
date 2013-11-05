a25832ea0f7e138a8cd5ea4ca9d01cc7441674b2
soum <somugus@gmail.com>
2013-11-05 16:06:52 -0500

    user agent

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,21 +1,11 @@
-//console.log('This would be the main JS file.');
 $(function(){
-if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
-    if (browserRatio >=1.5) {
-        //$container.css('min-height', '360px');
-		//alert('min-height 360px')
-		$('body').css('background', 'green')
-    } else {
-        //$container.css('min-height', '555px');
-		$('body').css('background', 'red')
-    }
-}
-
 var ua = navigator.userAgent.toLowerCase();
-var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+var isAndroid = ua.indexOf('android') > -1; //&& ua.indexOf("mobile");
+var isIos = ua.indexOf('ipad' || 'iphone') > -1;
 if(isAndroid) {
-  // Do something!
-  // Redirect to Android-site?
   alert('android')
+}
+if(isIos){
+	alert('ios')
 }
 });
